7b7997f3e543d6ec2f383dd451bc887c53f0020b
Sapan Bodiwala <sbodiwala10@gmail.com>
2015-04-09 22:43:24 -0400

    Created Factory for Colors, which adds to an array and displays on the page. Need to implement $interval and $timeout for Simon's turn on start game

--- HW2/scripts/app.js
+++ HW2/scripts/app.js
@@ -1,3 +1,58 @@
 //Swami Shreeji
 var myApp = angular.module('myApp', []);
 
+myApp.controller('myController', ['$scope', '$timeout', '$interval', 'BoardDTO', function($scope, $timeout, $interval, BoardDTO) {
+
+	$scope.board = new BoardDTO();
+	$scope.startGame = function() {
+		console.log("Starting Game!");
+	};
+	$scope.addNewColor = function(color) {
+		$scope.board.addColor(color);
+	};
+
+	//This is used for the total time it takes Simon to
+	//complete his turn
+	$timeout(function() {
+		console.log("Popat");
+	}, 2000);
+
+
+	//Use Interval to space out when the user selects a box
+	$interval(function() {
+		console.log("Popat Interval");
+	}, 4000);
+
+
+}]);
+
+
+myApp.factory('BoardDTO', function() {
+
+	function BoardDTO() {
+		this.colors = [];
+		this.score = '0';
+	};
+
+	BoardDTO.prototype.addColor = function(boxcolor) {
+		this.colors.push({
+			value: boxcolor
+		});
+		this.score++;
+		console.log(this.colors);
+	};
+
+	BoardDTO.prototype.getColors = function() {
+		return colors;
+	};
+
+	return BoardDTO;
+});
+
+
+myApp.service('something', function() {
+
+
+
+
+});
