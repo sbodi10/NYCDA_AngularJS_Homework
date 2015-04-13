a98fcde7515a451cbcc4a39e02f6217f158e1214
sbodi10 <sbodiwala10@gmail.com>
2015-04-13 10:50:47 -0400

    Added Factory.. Need to use Interval and Timeout Services

--- HW2/scripts/app.js
+++ HW2/scripts/app.js
@@ -1,27 +1,16 @@
 //Swami Shreeji
 var myApp = angular.module('myApp', []);
 
-myApp.controller('myController', ['$scope', '$timeout', '$interval', 'BoardDTO', function($scope, $timeout, $interval, BoardDTO) {
+myApp.controller('myController', ['$scope', '$timeout', '$interval', 'BoardDTO', 'SimonDTO', function($scope, $timeout, $interval, BoardDTO, SimonDTO) {
 
 	$scope.board = new BoardDTO();
 	$scope.startGame = function() {
 		console.log("Starting Game!");
+		var game = new SimonDTO();
 	};
 	$scope.addNewColor = function(color) {
 		$scope.board.addColor(color);
 	};
-
-	//This is used for the total time it takes Simon to
-	//complete his turn
-	$timeout(function() {
-		console.log("Popat");
-	}, 2000);
-
-
-	//Use Interval to space out when the user selects a box
-	$interval(function() {
-		console.log("Popat Interval");
-	}, 4000);
 
 
 }]);
@@ -47,12 +36,43 @@ myApp.factory('BoardDTO', function() {
 	};
 
 	return BoardDTO;
-});
-
-
-myApp.service('something', function() {
-
-
-
 
 });
+
+
+myApp.factory('SimonDTO', ['$timeout', '$interval', function($timeout, $interval) {
+
+	function SimonDTO() {
+		this.myPattern = [];
+		this.newPattern = [];
+		this.turn = 1;
+		this.seconds = 3000;
+	};
+
+	SimonDTO.selectColor = function() {
+
+
+	};
+
+	SimonDTO.prototype.myTurn = function() {
+
+		this.seconds+= 3000;
+
+		//This is used for the total time it takes Simon to
+		//complete his turn
+		$timeout(function(seconds) {
+
+
+			//Use Interval to space out when the user selects a box
+			$interval(function() {
+				console.log("Color Selected");
+			}, 1500);
+
+
+		}, this.seconds);
+
+	};
+
+
+	return SimonDTO;
+}]);
